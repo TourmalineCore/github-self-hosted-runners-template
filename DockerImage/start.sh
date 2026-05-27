9eda13d511e05bfa485f91e41f216cb13178b3c3
Maxim Rychkov <rychkov03@yandex.ru>
2026-05-27 10:40:59 +0500

    feat: add DinD config for amd64 and arm64 runners

---
+++ DockerImage/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /home/runner/actions-runner || exit
+
+./config.sh --url https://github.com/${REPOSITORY_OWNER} --token ${REG_TOKEN} --runnergroup $RUNNER_GROUP --labels $LABELS
+
+sudo /usr/local/bin/dind dockerd --log-level=error &
+
+cleanup() {
+  echo "Removing runner..."
+  ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
